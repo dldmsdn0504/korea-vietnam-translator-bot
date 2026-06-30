eb376eca6baa5faeee50c09b44e002758d8f667b
dldmsdn0504 <dldmsdn0504@gmail.com>
2026-07-01 04:27:23 +0700

    bot.py 업데이트

--- bot.py
+++ bot.py
@@ -10,19 +10,17 @@ app = Flask(__name__)
 def home():
     return "OK"
 
-def run_flask():
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
-
 TOKEN = os.environ.get("TOKEN")
 bot = telebot.TeleBot(TOKEN)
+
+def run_bot():
+    print("BOT START")
+    bot.infinity_polling(skip_pending=True, timeout=60, long_polling_timeout=60)
 
 @bot.message_handler(func=lambda message: True)
 def translate(message):
     try:
         text = message.text
-
-        if not text:
-            return
 
         if any("가" <= ch <= "힣" for ch in text):
             result = GoogleTranslator(source="auto", target="vi").translate(text)
@@ -35,8 +33,6 @@ def translate(message):
         print(e)
         bot.reply_to(message, "번역 오류")
 
-threading.Thread(target=run_flask, daemon=True).start()
+threading.Thread(target=run_bot).start()
 
-print("BOT START")
-
-bot.infinity_polling()
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
