90b698481fd600b3a5b5b98f771a9ed7fd2e8efe
dldmsdn0504 <dldmsdn0504@gmail.com>
2026-07-01 03:42:29 +0700

    bot.py 업데이트

--- bot.py
+++ bot.py
@@ -27,31 +27,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(func=lambda message: True)
 def translate(message):
-    try:
-        text = message.text
-
-        # 한국어 → 베트남어
-        if any("가" <= ch <= "힣" for ch in text):
-          target = "vi"
-
-        # 베트남어 포함 외국어 → 한국어
-        else:
-            target = "ko"
-
-
-        result = GoogleTranslator(
-            source="auto",
-            target=target
-        ).translate(text)
-
-
-        bot.reply_to(message, result)
-
-
-    except Exception as e:
-        print(e)
-        bot.reply_to(message, "번역 오류")
-
+    bot.reply_to(message, "테스트 성공")
 
 threading.Thread(
     target=flask_run,
